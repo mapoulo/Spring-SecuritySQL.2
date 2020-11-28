5f1316957bead1a2dce177a9b07869173b572198
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2020-11-28 12:58:54 +0200

    Encrypted password

--- SpringSecuritySQL.2/src/main/java/com/SpringSecuritySQL2/demo/config/MyConfiguration.java
+++ SpringSecuritySQL.2/src/main/java/com/SpringSecuritySQL2/demo/config/MyConfiguration.java
@@ -1,9 +1,10 @@
 package com.SpringSecuritySQL2.demo.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableConfigurationProperties
 public class MyConfiguration extends WebSecurityConfigurerAdapter {
@@ -12,4 +13,10 @@ public class MyConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 	}
+	
+	
+   @Bean
+   public BCryptPasswordEncoder passwordEncoder() {
+	   return new BCryptPasswordEncoder();
+   }
 }
--- SpringSecuritySQL.2/src/main/java/com/SpringSecuritySQL2/demo/controller/AdminController.java
+++ SpringSecuritySQL.2/src/main/java/com/SpringSecuritySQL2/demo/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.SpringSecuritySQL2.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,12 +15,21 @@ import com.SpringSecuritySQL2.demo.model.User;
 public class AdminController {
 	
 	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
 	private UserService userService;
 	
 	@RequestMapping(value="/adduser", method = RequestMethod.POST)
 	public String getUsers(@RequestBody User user) {
+		
+		String password = user.getUser_password();
+		String encrypedPassword = passwordEncoder.encode(password);
+		user.setUser_password(password);
 		userService.save(user);
 		return "The is added succefully ...";
 	}
+	
+	
 
 }
